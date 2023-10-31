a8368468e2b860a91f872da1d0ed8823e8aefda7
Gabriel Cardoso <cardosogabn@gmail.com>
2023-10-31 08:22:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bilaboard
+ 
---
+++ bilaapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BilaappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bilaapp'
