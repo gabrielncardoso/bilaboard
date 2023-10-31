12d4ffdea9f422c5c777912c4079e09375b3660c
Gabriel Cardoso <cardosogabn@gmail.com>
2023-10-31 09:38:39 -0300

    update 31oct
    created base models

--- bilaapp/models.py
+++ bilaapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    participants = models.ManyToManyField(User)
+
+class Game(models.Model):
+    name = models.CharField(max_length=50)
+    #TRY TO USE AN EXISTING DATABASE
+
+class Match(models.Model):
+    datetime = models.DateTimeField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+class Result(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    position = models.IntegerField()
